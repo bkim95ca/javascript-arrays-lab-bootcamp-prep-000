0a05f7a5552122c35d3f11cdf9ee23deebb91b2b
Brandon Kim <bkim95ca@gmail.com>
2018-02-27 23:27:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(kittens, "Ralph") {
-  kittens.push("Ralph")
+function destructivelyAppendKitten(kittens, ralph) {
+  kittens.push("ralph")
 
   return kittens
 }
